514c068fe517b50377f0cff50eafbe6fbefb9b02
Ryan Peterson <r.peterson104@gmail.com>
2019-06-11 19:42:45 -0700

    Attempt 1 of deploying react app...

---
+++ precache-manifest.41b6bda31553a391be49c9cccbf0bf38.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025f06c4f3f0c9362f010463877f5954",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d35763b47554129d730",
+    "url": "/static/css/main.bfce97d8.chunk.css"
+  },
+  {
+    "revision": "a0555bffee4cd7a7c4e7",
+    "url": "/static/js/2.ef75dfdf.chunk.js"
+  },
+  {
+    "revision": "5d35763b47554129d730",
+    "url": "/static/js/main.e192f5cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e668faa2fef0d9796e00958f280a9cae",
+    "url": "/static/media/chicken.e668faa2.svg"
+  },
+  {
+    "revision": "4b722d5e04e89504a4c8b1d3931bc26a",
+    "url": "/static/media/cow.4b722d5e.svg"
+  },
+  {
+    "revision": "54f3e23d984211a9f66ec32e1f852952",
+    "url": "/static/media/duck.54f3e23d.svg"
+  },
+  {
+    "revision": "745850082d098f4b402d1400eaa205b3",
+    "url": "/static/media/goat.74585008.svg"
+  },
+  {
+    "revision": "fd0fd8828b684cfba80d576853b6818f",
+    "url": "/static/media/goose.fd0fd882.svg"
+  },
+  {
+    "revision": "821d41f4347e2d2c38f1d1f8e8e46cd6",
+    "url": "/static/media/horse.821d41f4.svg"
+  },
+  {
+    "revision": "c2e39b78db01768c2841e13c0587e5b8",
+    "url": "/static/media/llama.c2e39b78.svg"
+  },
+  {
+    "revision": "5f2c34e0ac69458fc5656c630189091c",
+    "url": "/static/media/pig.5f2c34e0.svg"
+  },
+  {
+    "revision": "d2a6fafa06d459014903a14afa68b156",
+    "url": "/static/media/rabbit.d2a6fafa.svg"
+  },
+  {
+    "revision": "2d9e0cf425371c81fbc65cde698b8d74",
+    "url": "/static/media/turkey.2d9e0cf4.svg"
+  }
+]);
